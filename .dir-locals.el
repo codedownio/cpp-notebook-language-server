b3daf049bdd00a54ba94b554f6c3710760235b26
Tom McLaughlin <tom@codedown.io>
2026-01-05 15:50:34 -0800

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+(
+ (haskell-mode
+  .
+  (
+   (haskell-process-args-stack-ghci . ("--ghci-options=-ferror-spans"
+                                       "--no-build"
+                                       "--no-load"
+                                       "cpp-notebook-language-server:lib"
+                                       "cpp-notebook-language-server:exe:cpp-notebook-language-server"
+                                       ))
+   )
+  )
+ )
